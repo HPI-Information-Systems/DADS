4101cc3e54748d8797f1240a598674e3a3f9d8a5
Johannes Schneider <johannes.schneider@student.hpi.de>
2019-11-18 11:08:02 +0100

    initial project setup

---
+++ src/main/java/de/hpi/msc/jschneider/Main.java
@@ -0,0 +1,14 @@
+package de.hpi.msc.jschneider;
+
+import lombok.var;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        var test = "hello world";
+        System.out.println(test);
+    }
+
+}
