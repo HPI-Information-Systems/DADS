219419c429775a1bb06cf2215db16267dd8378bd
Johannes Schneider <johannes.schneider@student.hpi.de>
2020-02-14 10:13:35 +0100

    tiny refactoring

--- script/sequence-converter/convert.py
+++ script/sequence-converter/convert.py
@@ -3,14 +3,17 @@ import struct
 from pathlib import Path
 
 
-def main(arguments):
-    with arguments.input.open("r") as in_file, arguments.output.open("wb+") as out_file:
+def main():
+    parser = _create_arg_parser()
+    args = parser.parse_args()
+
+    with args.input.open("r") as in_file, args.output.open("wb+") as out_file:
         for line in in_file:
             value = float(line)
             out_file.write(bytearray(struct.pack("!d", value)))
 
 
-if __name__ == "__main__":
+def _create_arg_parser():
     argument_parser = argparse.ArgumentParser()
 
     argument_parser.add_argument(
@@ -25,5 +28,8 @@ if __name__ == "__main__":
         required=True
     )
 
-    arguments = argument_parser.parse_args()
-    main(arguments)
+    return argument_parser
+
+
+if __name__ == "__main__":
+    main()
